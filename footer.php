43255e0c93dc62f48f8a5fa40a1715901184e6b7
mohamedsekkaf <mohamedsekkaf343@gmail.com>
2021-03-24 01:45:49 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,46 @@
+
+
+ <footer id="footer">
+         
+         <div class="container">
+         <div class="row">
+              <div class="col-12">
+                    <p style="text-align:center;" class="small-text pull-lef "><font color='#8ab5d6'>&copy;  Ecole Supérieure de Technologie Sidi Bennour</font> </p>
+                    </div> <!-- /.col-md-5 -->
+                    <div class="col-12">
+                        <ul style="text-align:center; text-decoration: none;list-style-type:none" class="footer-nav drop-down">
+                            <li style="text-decoration: none;list-style-type:none" ><a href="actualite.php"><b>Actualité</b></a></li>
+                              <li style="text-decoration: none;list-style-type:none"><a href="contact.php"><b>Contact</b></a></li>
+                        </ul>
+                                 
+            </div> <!-- /.row -->
+            </div>
+     <div class="col-12 center">
+     </div>
+     <!-- mawa9i3 tawasol ijtima3i!!!!!!!! -->
+            <div class="head d-flex justify-content-center">
+                <div class="row" style="display: block!important;">
+                    <a href="https://facebook.com"><img class="circle" src="<?php echo get_template_directory_uri()?>/image/facebook.png" width="40" height="40"  /></a>
+                    <a href="https://www.instagram.com"><img class="circle" src="<?php echo get_template_directory_uri()?>/image/instagram.svg" width="40" height="40"/></a>
+                    <a href="estsb.ucd@gmail.com"> <img class="circle" src=""  /></a>
+                    <a href=""> <img class="circle" src="" /></a>
+                </div>
+            </div>
+        </div>
+         <!-- nihayat mawa9i3 tawasol ijtima3i!!!!!!!! -->
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong><span><?php echo '20'.date('y'); ?></span></strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                
+                Designed by <a href="#">MOHAMMED SEKKAF</a>
+                            
+            </div>
+        </div>
+    </footer><!-- End #footer -->
+
+
+<?php wp_footer();?>
+</body>
+</html>
